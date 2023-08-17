677b79447c0b319fc68d2374269a669ce1520ac5
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-08-17 13:54:16 -0400

    feat: unified connect button

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Dropdown } from "flowbite-react";
 import { CadtLogo } from "./components/CadtLogo";
 import { ExplorerLogo } from "./components/ExplorerLogo";
 import { TokenizationLogo } from "./components/TokenizationLogo";
-import { Flowbite } from "flowbite-react";
+import { Flowbite, Button, Modal } from "flowbite-react";
 import flowbiteThemeSettings from "./flowbite.theme";
 
 const appLinks = {
@@ -27,15 +27,10 @@ const appLinks = {
 
 const App = () => {
   const [activeApp, setActiveApp] = useState(appLinks["cadt"]);
+  const [showConnect, setShowConnect] = useState(false);
   const cadtRef = useRef(null);
   const climateExplorerRef = useRef(null);
   const climateTokenizationRef = useRef(null);
-
-  useEffect(() => {
-    if (cadtRef.current) {
-      window.cadt = cadtRef.current.contentWindow;
-    }
-  }, [cadtRef.current]);
 
   const themeSettings = useMemo(() => {
     return flowbiteThemeSettings();
@@ -46,72 +41,336 @@ const App = () => {
   const ExplorerLogo = appLinks["climateExplorer"].logo;
   const CadtLogo = appLinks["cadt"].logo;
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = document.getElementById("connectForm");
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    // Validation function for host
+    const isValidHost = (host) => /^https?:\/\/.+(:\d{1,5})?$/.test(host);
+    // Validation function for API key (adjust as needed)
+    const isValidApiKey = (key) => key.trim() !== "";
+
+    // Array containing the field names, corresponding error element IDs, and validation functions
+    const fields = [
+      {
+        name: "cadtRegistryHost",
+        errorId: "cadtRegistryHostError",
+        validation: isValidHost,
+      },
+      {
+        name: "cadtRegistryApiKey",
+        errorId: "cadtRegistryApiKeyError",
+        validation: isValidApiKey,
+      },
+      {
+        name: "climateTokenizationEngineHost",
+        errorId: "climateTokenizationEngineHostError",
+        validation: isValidHost,
+      },
+      {
+        name: "climateTokenizationEngineApiKey",
+        errorId: "climateTokenizationEngineApiKeyError",
+        validation: isValidApiKey,
+      },
+      {
+        name: "climateExplorerHost",
+        errorId: "climateExplorerHostError",
+        validation: isValidHost,
+      },
+    ];
+
+    let isValid = true;
+
+    fields.forEach((field) => {
+      const value = data[field.name];
+      const errorElement = document.getElementById(field.errorId);
+
+      if (!value) {
+        errorElement.textContent = "Field is required.";
+        isValid = false;
+      } else if (!field.validation(value)) {
+        errorElement.textContent =
+          field.validation === isValidHost
+            ? "Enter a valid URL with http:// or https://. Port numbers are allowed."
+            : "API key cannot be empty.";
+        isValid = false;
+      } else {
+        errorElement.textContent = ""; // Clear any previous error message
+      }
+    });
+
+    if (!isValid) return;
+
+    localStorage.setItem("connected", "true");
+    localStorage.setItem("cadtRemoteServerAddress", data.cadtRegistryHost);
+    localStorage.setItem("cadtRemoteServerApiKey", data.cadtRegistryApiKey);
+    localStorage.setItem(
+      "climateExplorerRemoteServerAddress",
+      data.climateExplorerHost
+    );
+    localStorage.setItem(
+      "climateTokenizationEngineRemoteServerAddress",
+      data.climateTokenizationEngineHost
+    );
+    localStorage.setItem(
+      "climateTokenizationEngineRemoteServerApiKey",
+      data.climateTokenizationEngineApiKey
+    );
+
+    setShowConnect(false);
+  };
+
+  const handleDisconnect = () => {
+    localStorage.removeItem("connected");
+    localStorage.removeItem("cadtRemoteServerAddress");
+    localStorage.removeItem("cadtRemoteServerApiKey");
+    localStorage.removeItem("climateExplorerRemoteServerAddress");
+    localStorage.removeItem("climateTokenizationEngineRemoteServerAddress");
+    localStorage.removeItem("climateTokenizationEngineRemoteServerApiKey");
+    setShowConnect(true);
+  };
+
+  function validateLocalStorage() {
+    const keys = [
+      "cadtRemoteServerAddress",
+      "cadtRemoteServerApiKey",
+      "climateExplorerRemoteServerAddress",
+      "climateTokenizationEngineRemoteServerAddress",
+      "climateTokenizationEngineRemoteServerApiKey",
+    ];
+
+    for (const key of keys) {
+      const value = localStorage.getItem(key);
+      if (value === null || value === "") {
+        // If any key fails the check, remove all keys and return false
+        keys.forEach((key) => localStorage.removeItem(key));
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   return (
     <Flowbite theme={themeSettings}>
       <div className="App">
-        <header className="App-header">
-          <Dropdown label={<ActiveLogo />} size="lg">
-            <Dropdown.Item onClick={() => setActiveApp(appLinks["cadt"])}>
-              <CadtLogo />
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => setActiveApp(appLinks["climateTokenization"])}
-            >
-              <TokenizationLogo />
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => setActiveApp(appLinks["climateExplorer"])}
-            >
-              <ExplorerLogo />
-            </Dropdown.Item>
-          </Dropdown>
+        <header
+          className="App-header px-4"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          {validateLocalStorage() ? (
+            <Dropdown label={<ActiveLogo />} size="lg">
+              <Dropdown.Item onClick={() => setActiveApp(appLinks["cadt"])}>
+                <CadtLogo />
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => setActiveApp(appLinks["climateTokenization"])}
+              >
+                <TokenizationLogo />
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => setActiveApp(appLinks["climateExplorer"])}
+              >
+                <ExplorerLogo />
+              </Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <div></div>
+          )}
+
+          {validateLocalStorage() ? (
+            <Button color="gray" onClick={handleDisconnect}>
+              Disconnect
+            </Button>
+          ) : (
+            <Button color="gray" onClick={() => setShowConnect(true)}>
+              Connect
+            </Button>
+          )}
+
+          {showConnect && (
+            <Modal show={true} onClose={() => setShowConnect(false)}>
+              <Modal.Header>Connect to Core Registry</Modal.Header>
+              <Modal.Body>
+                <form id="connectForm" onSubmit={handleSubmit}>
+                  <div className="space-y-8">
+                    <div className="space-y-4">
+                      <label className="block text-lg font-semibold text-gray-700">
+                        CADT Registry
+                      </label>
+                      <div className="space-y-2">
+                        <label className="block text-gray-600">
+                          Host <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="cadtRegistryHost"
+                          required
+                          pattern="https?://.+(:\d{1,5})?"
+                          className="w-full p-2 border rounded shadow-sm"
+                          placeholder="Enter CADT Registry Host URL"
+                        />
+                        <span
+                          id="cadtRegistryHostError"
+                          className="text-red-500"
+                        ></span>
+                      </div>
+                      <div className="space-y-2">
+                        <label className="block text-gray-600">
+                          API Key <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="cadtRegistryApiKey"
+                          required
+                          className="w-full p-2 border rounded shadow-sm"
+                          placeholder="Enter CADT Registry API Key"
+                        />
+                        <span
+                          id="cadtRegistryApiKeyError"
+                          className="text-red-500"
+                        ></span>
+                      </div>
+                    </div>
+
+                    <div className="space-y-4">
+                      <label className="block text-lg font-semibold text-gray-700">
+                        Climate Tokenization Engine
+                      </label>
+                      <div className="space-y-2">
+                        <label className="block text-gray-600">
+                          Host <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="climateTokenizationEngineHost"
+                          required
+                          pattern="https?://.+(:\d{1,5})?"
+                          className="w-full p-2 border rounded shadow-sm"
+                          placeholder="Enter Climate Tokenization Engine Host URL"
+                        />
+                        <span
+                          id="climateTokenizationEngineHostError"
+                          className="text-red-500"
+                        ></span>
+                      </div>
+                      <div className="space-y-2">
+                        <label className="block text-gray-600">
+                          API Key <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="climateTokenizationEngineApiKey"
+                          required
+                          className="w-full p-2 border rounded shadow-sm"
+                          placeholder="Enter Climate Tokenization Engine API Key"
+                        />
+                        <span
+                          id="climateTokenizationEngineApiKeyError"
+                          className="text-red-500"
+                        ></span>
+                      </div>
+                    </div>
+
+                    <div className="space-y-4">
+                      <label className="block text-lg font-semibold text-gray-700">
+                        Climate Explorer
+                      </label>
+                      <div className="space-y-2">
+                        <label className="block text-gray-600">
+                          Host <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                          type="text"
+                          name="climateExplorerHost"
+                          required
+                          pattern="https?://.+(:\d{1,5})?"
+                          className="w-full p-2 border rounded shadow-sm"
+                          placeholder="Enter Climate Explorer Host URL"
+                        />
+                        <span
+                          id="climateExplorerHostError"
+                          className="text-red-500"
+                        ></span>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button color="gray" onClick={() => setShowConnect(false)}>
+                  Cancel
+                </Button>
+                <Button color="primary" onClick={handleSubmit}>
+                  Connect
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          )}
         </header>
-        <div
-          className="app-window"
-          style={{
-            display:
-              activeApp.name === appLinks["cadt"].name ? "block" : "none",
-          }}
-        >
-          <iframe
-            ref={cadtRef}
-            src={appLinks["cadt"].link}
-            width="100%"
-            height="100%"
-          ></iframe>
-        </div>
-        <div
-          className="app-window"
-          style={{
-            display:
-              activeApp.name === appLinks["climateTokenization"].name
-                ? "block"
-                : "none",
-          }}
-        >
-          <iframe
-            ref={climateTokenizationRef}
-            src={appLinks["climateTokenization"].link}
-            width="100%"
-            height="100%"
-          ></iframe>
-        </div>
-        <div
-          className="app-window"
-          style={{
-            display:
-              activeApp.name === appLinks["climateExplorer"].name
-                ? "block"
-                : "none",
-          }}
-        >
-          <iframe
-            ref={climateExplorerRef}
-            src={appLinks["climateExplorer"].link}
-            width="100%"
-            height="100%"
-          ></iframe>
-        </div>
+
+        {validateLocalStorage() ? (
+          <>
+            <div
+              className="app-window"
+              style={{
+                display:
+                  activeApp.name === appLinks["cadt"].name ? "block" : "none",
+              }}
+            >
+              <iframe
+                ref={cadtRef}
+                src={appLinks["cadt"].link}
+                width="100%"
+                height="100%"
+              ></iframe>
+            </div>
+            <div
+              className="app-window"
+              style={{
+                display:
+                  activeApp.name === appLinks["climateTokenization"].name
+                    ? "block"
+                    : "none",
+              }}
+            >
+              <iframe
+                ref={climateTokenizationRef}
+                src={appLinks["climateTokenization"].link}
+                width="100%"
+                height="100%"
+              ></iframe>
+            </div>
+            <div
+              className="app-window"
+              style={{
+                display:
+                  activeApp.name === appLinks["climateExplorer"].name
+                    ? "block"
+                    : "none",
+              }}
+            >
+              <iframe
+                ref={climateExplorerRef}
+                src={appLinks["climateExplorer"].link}
+                width="100%"
+                height="100%"
+              ></iframe>
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-full">
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <h1 className="text-2xl font-semibold text-gray-700">
+                Welcome to Core Registry
+              </h1>
+              <p className="text-gray-600">Connect to get started</p>
+            </div>
+          </div>
+        )}
       </div>
     </Flowbite>
   );
--- src/flowbite.theme.js
+++ src/flowbite.theme.js
@@ -15,23 +15,26 @@ export default () => ({
           placement: "-4px",
         },
         base: "z-10 w-fit rounded divide-y divide-gray-100 shadow focus:outline-none",
-        content: "py-1 text-sm text-gray-700 dark:text-gray-200",
-        divider: "my-1 h-px bg-gray-100 dark:bg-gray-600",
-        header: "block py-2 px-4 text-sm text-gray-700 dark:text-gray-200",
+        content:
+          "py-1 text-sm text-gray-700 dark:text-gray-200 focus:outline-none",
+        divider: "my-1 h-px bg-gray-100 dark:bg-gray-600 focus:outline-none",
+        header:
+          "block py-2 px-4 text-sm text-gray-700 dark:text-gray-200 focus:outline-none",
         hidden: "invisible opacity-0",
         item: {
-          container: "",
-          base: "flex items-center justify-start py-2 px-4 text-sm text-gray-700 cursor-pointer w-full hover:bg-gray-900 focus:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-900 focus:outline-none dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white",
-          icon: "mr-2 h-4 w-4",
+          container: "focus:outline-none",
+          base: "focus:outline-none flex items-center justify-start py-2 px-4 text-sm text-gray-700 cursor-pointer w-full hover:bg-gray-900 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-900 focus:outline-none dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white",
+          icon: "mr-2 h-4 w-4 focus:outline-none",
         },
         style: {
-          dark: "bg-gray-900 text-white dark:bg-gray-700",
-          light: "border border-gray-500 bg-gray-900 text-gray-900",
-          auto: "border border-gray-500 bg-gray-900 text-gray-900 dark:border-none dark:bg-gray-700 dark:text-white",
+          dark: "bg-gray-900 text-white dark:bg-gray-700 focus:outline-none",
+          light:
+            "border border-gray-500 bg-gray-900 text-gray-900 focus:outline-none",
+          auto: "focus:outline-none border border-gray-500 bg-gray-900 text-gray-900 dark:border-none dark:bg-gray-700 dark:text-white",
         },
-        target: "w-fit",
+        target: "w-fit focus:outline-none",
       },
-      inlineWrapper: "flex items-center",
+      inlineWrapper: "flex items-center focus:outline-none",
     },
   },
 });
