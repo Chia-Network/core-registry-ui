a1c8e10e097007f172ab97f942e019beb0666300
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-09-20 13:03:10 -0400

    chore: bump tokenizatio engine

--- app-builds.js
+++ app-builds.js
@@ -8,7 +8,7 @@ module.exports = {
     url: "https://github.com/Chia-Network/climate-explorer-ui/releases/download/{{tag}}/climate-explorer-ui-web-build.tar.gz",
   },
   climate_tokenization_engine: {
-    tag: "1.1.4",
+    tag: "1.1.5",
     url: "https://github.com/Chia-Network/Climate-Tokenization-Engine-UI/releases/download/{{tag}}/climate-tokenization-engine-ui-web-build.tar.gz",
   },
 };
