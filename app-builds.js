e389bf1773f9a2c24145346c8283a2331f2331d8
Zachary Brown <z.brown@chia.net>
2024-08-14 10:10:50 -0700

    chore: bump app versions to support theme color chooser

--- app-builds.js
+++ app-builds.js
@@ -1,14 +1,14 @@
 module.exports = {
   cadt: {
-    tag: "1.3.9",
+    tag: "1.3.11",
     url: "https://github.com/Chia-Network/core-registry-cadt-ui/releases/download/{{tag}}/core-registry-cadt-ui-web-build.tar.gz",
   },
   climate_explorer: {
-    tag: "1.1.11",
+    tag: "1.1.14",
     url: "https://github.com/Chia-Network/climate-explorer-ui/releases/download/{{tag}}/climate-explorer-ui-web-build.tar.gz",
   },
   climate_tokenization_engine: {
-    tag: "1.1.8",
+    tag: "1.1.10",
     url: "https://github.com/Chia-Network/Climate-Tokenization-Engine-UI/releases/download/{{tag}}/climate-tokenization-engine-ui-web-build.tar.gz",
   },
 };
