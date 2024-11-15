a13064a81d1e368fdc868ef0a45537567a71e3c3
Zachary Brown <z.brown@chia.net>
2024-11-15 12:33:56 -0800

    chore: updated dependencies

--- app-builds.js
+++ app-builds.js
@@ -1,14 +1,14 @@
 module.exports = {
   cadt: {
-    tag: '1.3.11',
+    tag: '2.0.3',
     url: 'https://github.com/Chia-Network/core-registry-cadt-ui/releases/download/{{tag}}/core-registry-cadt-ui-web-build.tar.gz',
   },
   climate_explorer: {
-    tag: '1.1.14',
+    tag: '1.2.1',
     url: 'https://github.com/Chia-Network/climate-explorer-ui/releases/download/{{tag}}/climate-explorer-ui-web-build.tar.gz',
   },
   climate_tokenization_engine: {
-    tag: '1.1.10',
+    tag: '1.2.1',
     url: 'https://github.com/Chia-Network/Climate-Tokenization-Engine-UI/releases/download/{{tag}}/climate-tokenization-engine-ui-web-build.tar.gz',
   },
   climate_dashboard: {
